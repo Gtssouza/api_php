a77c13a6cad87d96c9762f5a558857f43ab4ae7f
Gustavo Souza <gtssouza94@gmail.com>
2020-11-17 20:03:49 -0300

    Iniciando modelagem de classes

--- src/routes/produtos.php
+++ src/routes/produtos.php
@@ -3,9 +3,15 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+/*
+ORM - Object Relation Mapper
+Illuminate - é o motor da base de dados do laravel sem o laravel
+Eloquent ORM
+*/
+
 //Rotas para produtos
 $app->group('/api/v1', function(){
-    $this->get('/produtos', function($request, $response){
+    $this->get('/produtos/lista', function($request, $response){
         return $response->withJson(['nome' => 'Moto G']);
     });
 });
