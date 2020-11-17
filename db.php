42db008aa9646039254212072ea6a89f66cdf108
Gustavo Souza <gtssouza94@gmail.com>
2020-11-17 18:03:28 -0300

    Criação de tabelas no Banco de Dados

--- db.php
+++ db.php
@@ -1,3 +1,33 @@
 <?php
-    
-?>
+if (PHP_SAPI != 'cli') {
+    exit('Rodar via CLI');
+}
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Instantiate the app
+$settings = require __DIR__ . '/src/settings.php';
+$app = new \Slim\App($settings);
+
+// Set up dependencies
+require __DIR__ . '/src/dependencies.php';
+
+$db = $container->get('db');
+
+$schema = $db->schema();
+$tabela = 'produtos';
+
+$schema->dropIfExists($tabela);
+
+//cria a tabela de produtos
+$schema->create($tabela, function($table){
+    //para criar consultar documentação do laravel
+    $table->increments('id');
+    $table->string('titulo', 100);
+    $table->text('descricao');
+    $table->decimal('preco', 11, 2);
+    $table->string('fabricante');
+    $table->date('dt_criacao');
+});
+
+
